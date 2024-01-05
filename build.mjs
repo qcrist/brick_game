87175fc3034e48d269bd1d3e9dd7d39f1caebc41
qcrist <qcrist@gmail.com>
2024-01-04 22:32:27 -0500

    dist => docs for github pages

--- build.mjs
+++ build.mjs
@@ -1,12 +1,12 @@
 import * as esbuild from 'esbuild'
 
-const production = process.argv[process.argv.length-1] === "production";
+const production = process.argv[process.argv.length - 1] === "production";
 
 const ctx = await esbuild.context({
     sourcemap: production ? undefined : "inline",
     entryPoints: ['src/index.tsx', 'src/main.css'],
     bundle: true,
-    outdir: 'dist',
+    outdir: 'docs',
     minify: production
 })
 
